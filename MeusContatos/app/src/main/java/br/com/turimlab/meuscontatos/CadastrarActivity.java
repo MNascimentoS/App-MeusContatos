833768c7bbf1361a11f5ac22efbf4fb9fbde56c1
Mateus <mateus.nnascimento.s@gmail.com>
2016-07-20 21:35:36 -0300

    Initial commit

---
+++ MeusContatos/app/src/main/java/br/com/turimlab/meuscontatos/CadastrarActivity.java
@@ -0,0 +1,40 @@
+package br.com.turimlab.meuscontatos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CadastrarActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText Nome;
+    EditText Endereco;
+    EditText Telefone;
+    EditText Email;
+    Button Cadastrar;
+    Button Cancelar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar_contato);
+        Cadastrar = (Button) findViewById(R.id.cadastrar);
+        Cadastrar.setOnClickListener(this);
+        Cancelar = (Button) findViewById(R.id.cancelar);
+        Cancelar.setOnClickListener(this);
+        Nome = (EditText) findViewById(R.id.nome);
+        Endereco = (EditText) findViewById(R.id.endereco);
+        Telefone = (EditText) findViewById(R.id.telefone);
+        Email = (EditText) findViewById(R.id.email);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == Cadastrar){
+
+        }else if (view == Cancelar){
+            finish();
+        }
+    }
+}
